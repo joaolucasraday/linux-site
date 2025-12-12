9f4ce0d450cfc6b9ee83fe21e8fb88dd89c6b3bd
João Lucas Raday <lucasraday@gmail.com>
2025-12-12 15:28:59 -0300

    base code

---
+++ linux-site/script.js
@@ -0,0 +1,11 @@
+const toggleButton = document.getElementById("toggle-dark");
+
+toggleButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+    
+    if(document.body.classList.contains("dark-mode")) {
+        toggleButton.textContent = "Light Mode";
+    } else {
+        toggleButton.textContent = "Dark Mode";
+    }
+});
